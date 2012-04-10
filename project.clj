48483466a650477a9144ade108c73e993ee72176
Max Penet <max.penet@gmail.com>
2012-04-10 15:10:48 +0200

    cleanup

--- project.clj
+++ project.clj
@@ -3,10 +3,8 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/core.incubator "0.1.0"]
                  [lamina "0.5.0-SNAPSHOT"]
-                 ;; [org.clojure/algo.monads "0.1.0"]
                  [org.apache.cassandra/cassandra-all "1.0.9"]
                  [tron "0.5.3"]
-                 [log4j/log4j "1.2.16"]
                  [commons-pool "1.6"]]
 
   :dev-dependencies [[codox "0.6.1"]]
