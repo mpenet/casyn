f509aafca052beeec234d087b7923fca7076774a
Max Penet <max.penet@gmail.com>
2012-05-08 00:33:29 +0200

    upgrade to cassandra 1.1.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/core.incubator "0.1.0"]
                  [lamina "0.5.0-SNAPSHOT"]
-                 [org.apache.cassandra/cassandra-all "1.0.9"]
+                 [org.apache.cassandra/cassandra-all "1.1.0"]
                  [tron "0.5.3"]
                  [commons-pool "1.6"]]
 
