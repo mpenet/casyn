fbe872119b014a768c100ae56d890fe62f146bb3
Max Penet <max.penet@gmail.com>
2012-05-09 13:57:32 +0200

    updade lamina to 0.5.0-alpha3

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Async Thrift based Cassandra Client"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/core.incubator "0.1.0"]
-                 [lamina "0.5.0-SNAPSHOT"]
+                 [lamina "0.5.0-alpha3"]
                  [org.apache.cassandra/cassandra-all "1.1.0"]
                  [tron "0.5.3"]
                  [useful "0.8.0"]
