26810652a7ea1abc6ed203f43bd627e7e9b96c2d
Max Penet <max.penet@gmail.com>
2012-07-08 11:01:04 +0200

    target cassandra 1.1.2

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/core.incubator "0.1.0"]
                  [lamina "0.5.0-alpha3"]
-                 [org.apache.cassandra/cassandra-all "1.1.0"]
+                 [org.apache.cassandra/cassandra-all "1.1.2"]
                  [tron "0.5.3"]
                  [useful "0.8.0"]
                  [commons-pool "1.6"]]
