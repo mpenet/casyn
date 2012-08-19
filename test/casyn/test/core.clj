263d1e62508c27d99a20b83575c778a781e7589a
Max Penet <max.penet@gmail.com>
2012-08-19 20:33:14 +0200

    use actual parameter in prepared statement

--- test/casyn/test/core.clj
+++ test/casyn/test/core.clj
@@ -321,9 +321,9 @@
         (c execute-cql-query "SELECT * FROM test_cf;")
         #(decode-result % test-codec-schema true)
         #(= "value0" (-> % ffirst val :n0))))
-  (let [prepared-statement @(c prepare-cql-query "SELECT * FROM test_cf;")]
+  (let [prepared-statement @(c prepare-cql-query "SELECT * FROM test_cf WHERE KEY=?;")]
       (is (not-empty prepared-statement))
       (is @(lc/run-pipeline
-            (c execute-prepared-cql-query (:item-id prepared-statement) [])
+            (c execute-prepared-cql-query (:item-id prepared-statement) ["0"])
             #(decode-result % test-codec-schema true)
             #(= "value0" (-> % ffirst val :n0))))))
