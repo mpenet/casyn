b1835ef86205a8a8f83b7c1189364f422640359d
Max Penet <max.penet@gmail.com>
2012-08-19 16:20:15 +0200

    update cassandra deps to 1.1.3

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/core.incubator "0.1.0"]
                  [lamina "0.5.0-SNAPSHOT"]
-                 [org.apache.cassandra/cassandra-all "1.1.2"]
+                 [org.apache.cassandra/cassandra-all "1.1.3"]
                  [tron "0.5.3"]
                  [useful "0.8.0"]
                  [commons-pool "1.6"]]
