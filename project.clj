5931d7fa8fdab29b86c3e5d98c9eed0a3e2a4804
Max Penet <max.penet@gmail.com>
2012-08-22 23:24:44 +0200

    update deps + stable version

--- project.clj
+++ project.clj
@@ -1,18 +1,16 @@
-(defproject cc.qbits/casyn "0.1.4-SNAPSHOT"
+(defproject cc.qbits/casyn "0.1.4"
   :description "Clojure client for Cassandra using Thrift AsyncClient + Lamina/perf"
   :url "https://github.com/mpenet/casyn"
   :license {:name "Eclipse Public License"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/core.incubator "0.1.0"]
-                 [lamina "0.5.0-SNAPSHOT"]
-                 [org.apache.cassandra/cassandra-all "1.1.3"]
+                 [lamina "0.5.0-beta1"]
+                 [org.apache.cassandra/cassandra-all "1.1.4"]
                  [tron "0.5.3"]
-                 [useful "0.8.0"]
+                 [useful "0.8.3"]
                  [commons-pool "1.6"]
                  [com.taoensso/nippy "0.10.1"]]
   :profiles {:1.4  {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5  {:dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}
              :dev  {:dependencies [[codox "0.6.1"]]}
              :test {:dependencies []}}
-  :min-lein-version "2.0.0"
-  :warn-on-reflection true)
+  :min-lein-version "2.0.0")
