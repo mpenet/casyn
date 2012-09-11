6fd0f6a0b12df21271d318b945eb47ba5a316e1f
Max Penet <m@qbits.cc>
2012-09-11 23:54:34 +0200

    back to snapshot for 0.1.7

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cc.qbits/casyn "0.1.6"
+(defproject cc.qbits/casyn "0.1.7-SNAPSHOT"
   :description "Clojure client for Cassandra using Thrift AsyncClient + Lamina/perf"
   :url "https://github.com/mpenet/casyn"
   :license {:name "Eclipse Public License"
