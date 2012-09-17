5fc0fe692d94123606a199dc4c4c136635ac054c
Max Penet <m@qbits.cc>
2012-09-17 21:07:59 +0200

    use latest lamina beta available

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [lamina "0.5.0-beta4"]
+                 [lamina "0.5.0-beta5"]
                  [org.apache.cassandra/cassandra-all "1.1.5"]
                  [tron "0.5.3"]
                  [useful "0.8.4"]
