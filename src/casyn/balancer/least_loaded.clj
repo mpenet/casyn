8038c351e4c39976e4130983ec595771ff4432ad
Max Penet <m@qbits.cc>
2012-10-25 09:13:18 +0200

    don't create new types for balancer instances, reify is good enough

--- src/casyn/balancer/least_loaded.clj
+++ src/casyn/balancer/least_loaded.clj
@@ -5,24 +5,21 @@
   (:import
    [java.util.concurrent LinkedBlockingQueue]))
 
-(deftype LeastLoadedBalancer [^LinkedBlockingQueue nodes]
-
-  PBalancer
-
-  (get-nodes [b]
-    (to-array nodes))
-
-  (select-node [b pool avoid-node-set]
-    (->> nodes
-         (remove avoid-node-set)
-         (sort-by (partial p/active-clients pool))
-         first))
-
-  (register-node [b node]
-    (.offer nodes node))
-
-  (unregister-node [b node]
-    (.remove nodes node)))
-
 (defmethod balancer :least-loaded [_ & opts]
-  (LeastLoadedBalancer. (LinkedBlockingQueue.)))
+  (let [nodes (LinkedBlockingQueue.)]
+    (reify PBalancer
+
+      (get-nodes [b]
+        (to-array nodes))
+
+      (select-node [b pool avoid-node-set]
+        (->> nodes
+             (remove avoid-node-set)
+             (sort-by (partial p/active-clients pool))
+             first))
+
+      (register-node [b node]
+        (.offer nodes node))
+
+      (unregister-node [b node]
+        (.remove nodes node)))))
--- src/casyn/balancer/round_robin.clj
+++ src/casyn/balancer/round_robin.clj
@@ -5,26 +5,22 @@
    [java.util.concurrent LinkedBlockingQueue]
    [java.util Collection]))
 
-(deftype RoundRobinBalancer [^LinkedBlockingQueue nodes]
-
-  PBalancer
-
-  (get-nodes [b]
-    (to-array nodes))
-
-  (select-node [b _ avoid-node-set]
-    (let [node (.take nodes)]
-      (.offer nodes node) ;; complete rotation
-      (if (contains? avoid-node-set node)
-        (.select-node b _ avoid-node-set)
-        node)))
-
-  (register-node [b node]
-    (.offer nodes node))
-
-  (unregister-node [b node]
-    (.remove nodes node)))
-
-
 (defmethod balancer :round-robin [_ & opts]
-  (RoundRobinBalancer. (LinkedBlockingQueue.)))
+  (let [nodes (LinkedBlockingQueue.)]
+    (reify PBalancer
+
+      (get-nodes [b]
+        (to-array nodes))
+
+      (select-node [b _ avoid-node-set]
+        (let [node (.take nodes)]
+          (.offer nodes node) ;; complete rotation
+          (if (contains? avoid-node-set node)
+            (.select-node b _ avoid-node-set)
+            node)))
+
+      (register-node [b node]
+        (.offer nodes node))
+
+      (unregister-node [b node]
+        (.remove nodes node)))))
