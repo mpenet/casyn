44b72f33a66da53c68fa7925201b2acec31514bf
Max Penet <m@qbits.cc>
2012-11-14 18:46:24 +0100

    back to snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cc.qbits/casyn "0.9.4"
+(defproject cc.qbits/casyn "0.9.5-SNAPSHOT"
   :description "Cassandra client with support for asynchronous operations"
   :url "https://github.com/mpenet/casyn"
   :license {:name "Eclipse Public License"
