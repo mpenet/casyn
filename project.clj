049444404f7dbff67d0c4726df0cfca982929aaf
Max Penet <m@qbits.cc>
2013-02-05 10:12:14 +0100

    release 1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cc.qbits/casyn "1.0.0-rc7"
+(defproject cc.qbits/casyn "1.0.0"
   :description "Cassandra client with support for asynchronous operations"
   :url "https://github.com/mpenet/casyn"
   :license {:name "Eclipse Public License"
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [lamina "0.5.0-beta10"]
                  [org.apache.cassandra/cassandra-all "1.2.1"]
-                 [useful "0.8.6"]
+                 [useful "0.8.8"]
                  [commons-pool "1.6"]
                  [com.taoensso/nippy "1.0.1"]
                  [cc.qbits/tardis "0.3.1"]
