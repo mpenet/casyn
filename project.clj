0658659158f5f3d36464aafc8e23781e0b49b8a6
Max Penet <m@qbits.cc>
2013-04-18 13:24:50 +0200

    bump version

--- project.clj
+++ project.clj
@@ -1,16 +1,16 @@
-(defproject cc.qbits/casyn "1.2.1"
+(defproject cc.qbits/casyn "1.2.2"
   :description "Cassandra client with support for asynchronous operations"
   :url "https://github.com/mpenet/casyn"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [lamina "0.5.0-beta15"]
-                 [org.apache.cassandra/cassandra-all "1.2.3"]
+                 [org.apache.cassandra/cassandra-all "1.2.4"]
                  [useful "0.8.8"]
                  [commons-pool "1.6"]
                  [com.taoensso/nippy "1.2.0"]
                  [cc.qbits/tardis "0.3.1"]
-                 [cc.qbits/hayt "0.4.0-beta3"]
+                 [cc.qbits/hayt "0.4.0-beta4"]
                  [cc.qbits/knit "0.2.1"]]
   :repositories {"sonatype-oss-public"
                  "https://oss.sonatype.org/content/groups/public/"}
